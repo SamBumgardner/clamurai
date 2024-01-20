ee3b0faa377dd538edef0b692c3f8f558a360917
Sam Bumgardner <Sambumgardner1@gmail.com>
2024-01-20 17:34:41 -0600

    implemented on_ground check in player
    Uses two raycasts for each side of the player (would be better to not hard-code the width, but it's workable). This strategy helps us avoid a player being considered 'off of the ground' by being half-off of a surface.

--- clamurai/Assets/Scripts/Player.cs
+++ clamurai/Assets/Scripts/Player.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -9,6 +10,8 @@ public class Player : MonoBehaviour
     private StateMachine stateMachine = new StateMachine();
     private List<State> states = new List<State>();
     public Rigidbody2D rb;
+
+    public bool on_ground = false;
 
     // Start is called before the first frame update
     void Start()
@@ -36,12 +39,31 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+
         applyInputAndTransitionStates();
         stateMachine.CurrentState.LogicUpdate();
     }
 
     private void FixedUpdate()
     {
+
+        var new_isOnGround = IsOnGround();
+        if (new_isOnGround != on_ground)
+        {
+            on_ground = new_isOnGround;
+            print("woah, on_ground is now " + on_ground.ToString());
+        }
+
         stateMachine.CurrentState.PhysicsUpdate();
+    }
+
+    public bool IsOnGround()
+    {
+        Debug.DrawRay(transform.position + new Vector3(.5f, 0, 0), Vector2.down * .55f, Color.green);
+        Debug.DrawRay(transform.position - new Vector3(.5f, 0, 0), Vector2.down * .55f, Color.green);
+        var hitLeft = Physics2D.Raycast(transform.position + new Vector3(.5f, 0, 0), Vector2.down, .55f, LayerMask.GetMask("Terrain"));
+        var hitRight = Physics2D.Raycast(transform.position - new Vector3(.5f, 0, 0), Vector2.down, .55f, LayerMask.GetMask("Terrain"));
+        return hitLeft.collider != null || hitRight.collider != null;
     }
 }
