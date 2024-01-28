a33878ec3b7340ca547957a5ab42eb81066de443
Jake LaCombe <jake.lacombe2@gmail.com>
2024-01-27 22:49:38 -0600

    Improve Jumping Mechanics

--- clamurai/Assets/Scripts/FSM/PlayerStates/AirState.cs
+++ clamurai/Assets/Scripts/FSM/PlayerStates/AirState.cs
@@ -1,9 +1,22 @@
+using UnityEngine;
+
 public class AirState : State<Player>
 {
 	protected AirState(Player player, StateMachine<Player> stateMachine) : base(player, stateMachine) { }
 
     public override int HandleInput()
     {
+        float horizontalAxis = Input.GetAxis("Horizontal");
+        owner.rb.velocity = new Vector2(Player.RUN_SPEED * horizontalAxis, owner.rb.velocity.y);
+
+        if (horizontalAxis != 0.0f)
+        {
+            var newXScale = horizontalAxis > 0 ? 1 : -1;
+            var scale = owner.transform.localScale;
+            scale.x = newXScale;
+            owner.transform.localScale = scale;
+        }
+
         // Velocity check makes sure this doesn't glue player back to ground when starting jump
         // Value doesn't matter too much, just check its positive and somewhat larger than 0.
         if (owner.IsOnGround() && owner.rb.velocity.y <= 1) 
